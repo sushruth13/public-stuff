9e4a9038093a71247dd17e9902df8095b09041a5
sushruth13 <37882612+sushruth13@users.noreply.github.com>
2022-09-14 13:16:50 -0400

    Add files via upload

---
+++ kubernetes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ufw disable
+swapoff -a; sed -i '/swap/d' /etc/fstab
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+{
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+}
+sudo apt-get update && sudo apt-get install kubelet kubeadm kubectl -y && sudo apt-hold mark kubectl kubeadm kubectl 
